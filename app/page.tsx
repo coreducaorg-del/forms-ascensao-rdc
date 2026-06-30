01243dbc942ae3b98155ce53884cb5c9bdaf9fd8
Focus Club <focusclub.adm@gmail.com>
2026-06-30 15:21:51 -0300

    adiciona redirecionamento condicional para dominio aulaodecoreano.online

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 
 export default function Home() {
+  const headersList = headers();
+  const host = headersList.get("host") || "";
+
+  if (host.includes("aulaodecoreano.online")) {
+    redirect("/baseinterna");
+  }
+
   redirect("/formulario");
 }
